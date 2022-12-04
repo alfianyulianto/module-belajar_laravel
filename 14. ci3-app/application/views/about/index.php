2a5c99e91a4b2f128a24901f46088f605568f625
Alfian Yulianto <48283799+alfianyulianto@users.noreply.github.com>
2022-12-04 12:33:09 +0700

    Delete 14. ci3-app directory

--- 14. ci3-app/application/views/about/index.php
+++
@@ -1,9 +0,0 @@
-<div class="container">
-  <div class="row mt-4">
-    <div class="col-6">
-      <h2>About Me</h2>
-      <img src="<?= base_url(); ?>assets/img/alfian.jpg" alt="Alfian Yulianto" width="80px" height="100px">
-      <p>Halo nama saya <?= $nama; ?> saya adalah seorang <?= $pekerjaan; ?></p>
-    </div>
-  </div>
-</div>
--- 14. ci3-app/application/views/home/index.php
+++
@@ -1,2 +0,0 @@
-<!-- $data['judul'] yd dikirim ke view akan diekstrak dan diambil keynya untuk dijadikan variabel -->
-<h1>Hello, <?= $nama; ?>!</h1>
